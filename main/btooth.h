d9f95b3edd55aad1f2a557a9b039b66b025476c3
antivorg <antivorg@yahoo.co.uk>
2021-02-19 15:09:44 +0000

    Created the rought outline of blue tooth protocol

--- main/btooth.h
+++ main/btooth.h
@@ -1,18 +1,72 @@
+#include <stdarg.h>
+#include <stdlib.h>
 
 typedef struct session;
 
+// Top level functions
 void btoothStartOuting(session* currentOuting);
 void btoothUpdateOuting(session* currentOuting);
 void btoothUploadOuting(session* currentOuting);
 
+// Lower level functions
+int pairedBool(char* signifier);
+
+
+// Top level functions //
+
 void btoothStartOuting(session* currentOuting) {
-    btoothSerial.write("signifier");
+    btoothSerial.write("Start");
+    pairedBool("Start");
+    writeData(currentOuting->startTime);
 }
 
 void btoothUpdateOuting(session* currentOuting) {
-    btoothSerial.write("signifier");
+    btoothSerial.write("Update");
+    pairedBool("Update");
+    writeData((double) currentOuting->stepNum,
+                (double) currentOuting->distance,
+                currentOuting->timeWaling,
+                currentOuting->timeRunning);
 }
 
 void btoothUploadOuting(session* currentOuting) {
-    btoothSerial.write("signifier");
+    btoothSerial.write("Upload");
+    pairedBool("Upload");
+    writeData(currentOuting->endTime,
+                (double) currentOuting->stepNum,
+                (double) currentOuting->distance,
+                currentOuting->timeWaling,
+                currentOuting->timeRunning);
+}
+
+
+// Lower level functions //
+
+int pairedBool(char* signifier) {
+
+    // Will be adding a time interupt to exit if it takes too long
+
+    int pairedBool = 0;
+    while(!pairedBool) {
+        // Wait for a response
+        while(!btoothSerial.avaliable());
+        // Allow time so signifier to load onto buffer
+        delay(500);
+        if (btoothSerial.read() == signifier) {
+            return 1;
+        } else {
+            continue;
+        }
+    }
+}
+
+void writeData(double args, ...) {
+    va_list vaList;
+    va_start(vaList, args);
+    while (args) {
+        bToothSerial.write(args);
+        bToothSerial.write(",");
+        args = va_arg(vaList, double);
+    }
+    bToothSerial.write("*");
 }
