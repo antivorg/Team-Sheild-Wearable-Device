ed17b2cd3e38065cab94034ee520d24969a4f3c7
antivorg <antivorg@yahoo.co.uk>
2021-03-15 16:54:47 +0000

    finalised code submission

--- ActualSubmission/ActualSubmission.ino
+++ ActualSubmission/ActualSubmission.ino
@@ -39,7 +39,7 @@ int times=0;
 //coordinates s; // displacement
 //coordinates u; // velocity of the last milisecond
 //coordinates v; // velocity of this milisecond
-//coordinates acceleration; // acceleration of this milisecond
+coordinates acceleration; // acceleration of this milisecond
 //float t = 0.001; // how long the interupt is, 1 milisecond
 int steps = 0; // step counter
 //int walking = 0; //the boolean one for deciding walking/running
@@ -65,8 +65,8 @@ void setup() {
     Serial.begin(9600);
     while(!Serial);
     GPSserial.begin(9600);
-    pinMode(9, Output);
-    pinMode(10, Output);
+    pinMode(9, OUTPUT);
+    pinMode(10, OUTPUT);
 
     pinMode(A0 , INPUT); //a.x
     pinMode(A1 , INPUT); //a.y
@@ -84,7 +84,11 @@ void loop() {
     if (watchWithinHouse()) {
         // device is inside thus wait
         delay(10*k);
+        digitalWrite(9, HIGH);
+        digitalWrite(10, LOW);
     } else {
+        digitalWrite(9, LOW);
+        digitalWrite(10, HIGH);
         // outside thus outing begins
         packet currentPacket = createPacket();
         bToothUpdate(&currentPacket);
@@ -93,10 +97,10 @@ void loop() {
             countSteps(10*k);
             currentPacket = createPacket();
             bToothUpdate(&currentPacket);
-            prevStepCount = steps;
         }
         currentPacket = createPacket();
         bToothUpdate(&currentPacket);
+        steps = 0;
     }
 
 }
@@ -106,8 +110,8 @@ void loop() {
 
 void countSteps(int interuptRunTime) {
     sei();
-    while(runTime != interuptRunTime);
     runTime = 0;
+    while(runTime != interuptRunTime);
     cli();
 }
 
@@ -120,74 +124,71 @@ SIGNAL(TIMER0_COMPA_vect) {
 
 
 void getAcceleration(){
-  x[times%25] = analogRead(A0);
-  if(times%25==0){
-    for(int i=0;i<100;i++){
-      totalx+=x[i];
+    int totalx, totaly, totalz;
+    x[times%25] = analogRead(A0);
+    if(times%25==0){
+        for(int i=0;i<100;i++){
+            totalx+=x[i];
+        }
+        acceleration.x=totalx/25;
+        if(acceleration.x<0){
+            acceleration.x=4.65*(9.81/0.33)*(acceleration.x+323)/1023;
+        }
+        else{
+            acceleration.x=4.65*(9.81/0.33)*(acceleration.x-324)/1023;
+        }
+        totalx=0;
+        if(abs(acceleration.x)<0.3){
+            acceleration.x=0;
+        }
     }
-    acceleration.x=totalx/25;
-    if(acceleration.x<0){
-      acceleration.x=4.65*(9.81/0.33)*(acceleration.x+323)/1023;
+    y[times%25] = analogRead(A1);
+    if(times%25==0){
+        for(int i=0;i<100;i++){
+            totaly+=y[i];
+        }
+        acceleration.y=(totaly/25)+320;
+        acceleration.y=4.65*(9.81/0.33)*(acceleration.y)/1023;
+        if(acceleration.y<0){
+            acceleration.y+=0.5;
+        } else {
+        //acceleration.y-=3;
+        }
+        totaly=0;
+        if(abs(acceleration.y)<0.16){
+            acceleration.y=0;
+        }
     }
-    else{
-      acceleration.x=4.65*(9.81/0.33)*(acceleration.x-324)/1023;
+    z[times%25] = analogRead(A2);
+    if(times%25==0){
+        for(int i=0;i<100;i++){
+            totalz+=z[i];
+        }
+        acceleration.z = totalz/25;
+        acceleration.z=4.65*(9.81/0.33)*(acceleration.z-270)/1023;
+        if(acceleration.z<0){
+            acceleration.z+=0.41;
+        } else {
+        acceleration.z-=0.41;
+        }
+        totalz=0;
+        if(abs(acceleration.z)<0.16){
+            acceleration.z=0;
+        }
     }
-    totalx=0;
-    if(abs(acceleration.x)<0.3){
-      acceleration.x=0;
+    acceleration.magnitude = sqrt(sq(acceleration.x) + sq(acceleration.y) + sq(acceleration.z));
+    if(abs(acceleration.x)>1.3 && times>600){
+        digitalWrite(13, HIGH);
+        steps++;
+        times=0;
+        Serial.println(steps);
+        Serial.println(acceleration.x);
+    } else if (times==500){
+        digitalWrite(13, LOW);
+        times++;
+    } else {
+        times++;
     }
-  }
-  y[times%25] = analogRead(A1);
-  if(times%25==0){
-    for(int i=0;i<100;i++){
-      totaly+=y[i];
-    }
-    acceleration.y=(totaly/25)+320;
-    acceleration.y=4.65*(9.81/0.33)*(acceleration.y)/1023;
-    if(acceleration.y<0){
-      acceleration.y+=0.5;
-    }
-    else{
-      //acceleration.y-=3;
-    }
-    totaly=0;
-    if(abs(acceleration.y)<0.16){
-      acceleration.y=0;
-    }
-  }
-  z[times%25] = analogRead(A2);
-  if(times%25==0){
-    for(int i=0;i<100;i++){
-      totalz+=z[i];
-    }
-    acceleration.z = totalz/25;
-    acceleration.z=4.65*(9.81/0.33)*(acceleration.z-270)/1023;
-    if(acceleration.z<0){
-      acceleration.z+=0.41;
-    }
-    else{
-      acceleration.z-=0.41;
-    }
-    totalz=0;
-    if(abs(acceleration.z)<0.16){
-      acceleration.z=0;
-    }
-  }
-  acceleration.magnitude = sqrt(sq(acceleration.x) + sq(acceleration.y) + sq(acceleration.z));
-  if(abs(acceleration.x)>1.3 && times>600){
-      digitalWrite(13, HIGH);
-      steps++;
-      times=0;
-      Serial.println(steps);
-      Serial.println(acceleration.x);
-  }
-  else if(times==500){
-    digitalWrite(13, LOW);
-    times++;
-  }
-  else{
-    times++;
-  }
 }
 
 packet createPacket(void) {
@@ -261,16 +262,13 @@ packet createPacket(void) {
     while (tokens != NULL) {
         switch (tokensIndex) {
         case 0:
-            Serial.printl(tokens);
             currentPacket.time = atof(tokens);
         case 1:
-            Serial.printl(tokens);
-            double lat = atof(tokens)
+            double lat = atof(tokens);
             currentPacket.latitude = (int) (lat/100) + ((lat/100) - ((int) lat /100))*100 / 60;
         case 2:
             if (*tokens == 'S') currentPacket.latitude = -currentPacket.latitude;
         case 3:
-            Serial.printl(tokens);
             double lon = atof(tokens);
             currentPacket.longitude = (int) (lon/100) + ((lon/100) - ((int) lon /100))*100 / 60;
         case 4:
@@ -287,7 +285,7 @@ packet createPacket(void) {
 
 int watchWithinHouse(void) {
     packet currentPacket = createPacket();
-    const int coef 50;
+    const int coef = 14.28595;
     if (abs(homeLatitude-currentPacket.latitude)<homeRadius/coef) {
         return 1;
     } else if (abs(homeLongitude-currentPacket.longitude)<homeRadius/coef) {
@@ -305,5 +303,5 @@ void bToothUpdate(packet* currentPacket) {
     Serial.print("long");
     Serial.print(currentPacket->longitude);
     Serial.print("lati");
-    Serial.print(currentPacket->latitue);
+    Serial.print(currentPacket->latitude);
 }
